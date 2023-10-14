227baf29d1e9c094cee005a290998fec8335abf7
Hailey <Hexmaster111@gmail.com>
2023-10-14 17:19:24 -0500

    added some code

---
+++ code_gen_playtime/codegen/Program.cs
@@ -0,0 +1,12 @@
+﻿//Sample Generated Code//
+namespace ConsoleApp{
+    internal class Program{
+        static int Main(string[] args){
+            int _gen_a = Int32.Parse(args[0]);
+            int _gen_b = Int32.Parse(args[1]);
+            int _gen_c = _gen_a + _gen_b;
+            Console.WriteLine(string.Format("{0}+{1}={2}", _gen_a, _gen_b, _gen_c));
+            return _gen_c;
+        }
+    }
+}
